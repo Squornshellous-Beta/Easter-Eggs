44eb04eb7e08dfcb78ceea577d5b2b426ab076d9
Squornshellous-Beta <69040653+Squornshellous-Beta@users.noreply.github.com>
2024-06-22 18:57:44 +0100

    Update mod.js
    Added extra items.

--- mod.js
+++ mod.js
@@ -2,7 +2,7 @@ module.exports = {
 	title:"Easter Eggs",
 	summary:"Notifies the reader of hidden easter eggs in interactive Flashes.",
 	author:"Squornshellous Beta",
-	modVersion:1.0,
+	modVersion:1.1,
 	
 	edit(archive) {
 		for (var i in eggs) archive.mspa.story[i].content+="<br /><br /><i>(This page contains an easter egg! "+eggs[i]+")</i>";
@@ -18,5 +18,10 @@ eggs={
 	"006273":"Click the link in the corner at the end to activate.",
 	"006517":"Click the link in the corner at the end to activate.",
 	"006927":"Click the link in the corner at the end to activate.",
-	"007298":"Visit the exposition booth as Damara to activate."
+	"007298":"Visit the exposition booth as Damara to activate.",
+	"007931":"Click the replay button at the end of the flash to activate.",
+	"007946":"Catch the replay button at the end of the flash to activate.",
+	"007966":"Click the replay button at the end of the flash to activate.",
+	"007973":"Click the replay buttons at the end of the flash to activate.",
+	"008143":"Click the ==> before the end of the flash to activate. (There are two messages during the song, and one during the applause.)",
 };
